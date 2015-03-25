d485c597450464f12f3a94236179af7676fa2836
luisgiltajamar <luis.gil@tajamar365.com>
2015-03-25 16:00:48 +0000

    A

---
+++ ManejoListas/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c97c231e-2651-4d0f-97bc-9a5d694461aa" featureId="c97c231e-2651-4d0f-97bc-9a5d694461aa" imageUrl="" scope="Site" solutionId="00000000-0000-0000-0000-000000000000" title="ManejoListas Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="17fffd93-45f1-47c9-8791-015cac39ecd4" />
+  </projectItems>
+</feature>
---
+++ ManejoListas/VisualWebPart1/VisualWebPart1.ascx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+
+namespace ManejoListas.VisualWebPart1
+{
+    [ToolboxItemAttribute(false)]
+    public partial class VisualWebPart1 : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling on a farm solution
+        // using the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public VisualWebPart1()
+        {
+        }
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            using (var web=SPContext.Current.Web)
+            {
+               lstListas.Items.Clear();
+                foreach (SPList lista in web.Lists)
+                {
+                    lstListas.Items.Add(new ListItem(lista.Title,
+                        lista.ID.ToString()));
+                }
+
+            }
+        }
+
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void lstItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstItems.SelectedIndex != -1)
+            {
+               using (SPWeb web=SPContext.Current.Web)
+               {
+                   var lista = ViewState["lista"] as string;
+
+                   var list = web.Lists[lista];
+                   var item = list.Items[new Guid(lstItems.SelectedValue)];
+
+                   informacion.Text = "";
+                   foreach (SPField field in item.Fields)
+                   {
+                       informacion.Text += field.Title + "-->" + 
+                           field.ToString();
+                   }
+               }
+
+            }
+
+
+        }
+
+        protected void lstListas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstListas.SelectedIndex != -1)
+            {
+               
+                using (SPWeb web=SPContext.Current.Web)
+                {
+                    String select = lstListas.SelectedItem.Text;
+                    ViewState["lista"] = select;
+                    SPList lista = web.Lists[select];
+                    lstItems.Items.Clear();
+                    foreach (SPListItem item in lista.Items)
+                    {
+                        lstItems.Items.Add(new ListItem(item.Name,
+                            item.UniqueId.ToString()));
+                    }
+                }
+            }
+        }
+    }
+}
